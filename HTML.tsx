bddb3d12cd3acd3ee96c5d597440d79c1d7ba13b
Siddesh Patil <coolasid123@gmail.com>
2024-05-04 16:25:23 +0530

    added html and css files

---
+++ HTML.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './CssFile.css';
+
+const HTML = () => {
+  return (
+    <>
+      <div className="outerCont">
+        <div>One</div>
+        <div>two</div>
+        <div>three</div>
+        <div>four</div>
+        <div>five</div>
+        <div>six</div>
+        <div>seven</div>
+        <div>eight</div>
+        <div>nine</div>
+        <div>ten</div>
+        <div>eleven</div>
+        <div>twelve</div>
+      </div>
+    </>
+  );
+};
+
+export default HTML;
